bc06eeb25610090f4b1aa2213c242280c001f419
Scott Kuo <scott@scottkuo.com>
2020-11-22 03:31:54 -0800

    Added new remote programming and calling commands

--- app.js
+++ app.js
@@ -6,53 +6,117 @@ const sessionio = require('socket.io')(server);
 const tallyio = require('socket.io-client');
 const os = require('os');
 const ifaces = os.networkInterfaces();
-
-var TallySocket;
-
-server.listen(3778);
-
-bonjour.publish({ name: "ATEM Tally Pi Listener", type: "dsft-tally-pi", port: 3778 });
+const { v4: uuidv4 } = require('uuid');
 
 const programLed = new Gpio(config.programGpio, 'out');
 const previewLed = new Gpio(config.previewGpio, 'out');
 
-function exitHandler(options, exitCode) {
-    programLed.write( 0 );
-    previewLed.write( 0 );
+var TallySocket;
+var lastState;
+var deviceId;
+
+const setDevId = function() {
+    console.log("Generating new ID");
+    return uuidv4();
+}
+
+const getDevId = function() {
+    if (!deviceId || deviceId === null) {
+        deviceId = setDevId();
+    }
+    console.log("Device ID" + deviceId);
+    return deviceId;
+}
+
+const updateTally = function() {
+    programLed.write(0);
+    previewLed.write(0);
+    if (!lastState.programSourceIds || !lastState.previewSourceIds)
+        return;
+
+    if (lastState.programSourceIds.includes(config.camera)) {
+        programLed.write(1);
+    } else if (lastState.previewSourceIds.includes(config.camera)) {
+        previewLed.write(1);
+    }
+}
+
+const exitHandler = function(options, exitCode) {
+    programLed.write(0);
+    previewLed.write(0);
     if (options.cleanup) console.log('clean');
     if (exitCode || exitCode === 0) console.log(exitCode);
     if (options.exit) process.exit();
 }
 
+server.listen(3778);
+
+config.camera = 1;
+
+bonjour.publish({
+    name: "ATEM Tally Pi Listener",
+    type: "dsft-tally-pi",
+    port: 3778,
+    txt: {
+        id: getDevId()
+    }
+});
+
 //do something when app is closing
-process.on('exit', exitHandler.bind(null,{cleanup:true}));
-process.on('SIGINT', exitHandler.bind(null, {exit:true}));
-process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
-process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
-process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
-        
+process.on('exit', exitHandler.bind(null, { cleanup: true }));
+process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
+
 sessionio.on('connection', (socket) => {
 
-	socket.on('pi_host_connect', function(msg) {
+    socket.on('pi_host_connect', function(msg) {
 
-		if (TallySocket)
+        if (TallySocket) {
+            console.log("Already have an active connection");
             return;
+        }
 
         var host = msg;
 
         TallySocket = new tallyio(host);
-        
-        TallySocket.on('connect', function (){
+
+        TallySocket.on('connect', function() {
             console.log("Connected to server ");
         });
 
         TallySocket.on('update_tally', function(msg) {
-            programLed.write( 0 );
-            previewLed.write( 0 );
-            if (msg.programSourceIds.includes(config.camera)) {
-                programLed.write( 1 );
-            } else if (msg.previewSourceIds.includes(config.camera)) {
-                previewLed.write( 1 );
+            lastState = msg;
+            updateTally();
+        });
+
+        TallySocket.on('call', function(msg) {
+            programLed.write(0);
+            setTimeout(() => { programLed.write(1); }, 250);
+            setTimeout(() => { programLed.write(0); }, 500);
+            setTimeout(() => { programLed.write(1); }, 750);
+            setTimeout(() => { programLed.write(0); }, 1000);
+            setTimeout(() => { updateTally(); }, 1000);
+        });
+
+        TallySocket.on('set_remote', function(msg) {
+            if (msg.devId == getDevId() || msg.devId == '*') {
+                if (msg.camera) {
+                    config.camera = msg.camera;
+                }
+
+                if (msg.identify) {
+                    programLed.write(0);
+                    setTimeout(() => { programLed.write(1); }, 250);
+                    setTimeout(() => { programLed.write(0); }, 500);
+                    setTimeout(() => { programLed.write(1); }, 750);
+                    setTimeout(() => { programLed.write(0); }, 1000);
+                    setTimeout(() => { programLed.write(1); }, 1250);
+                    setTimeout(() => { programLed.write(0); }, 1500);
+                    setTimeout(() => { programLed.write(1); }, 1750);
+                    setTimeout(() => { updateTally(); }, 2000);
+                }
             }
         });
 
@@ -68,11 +132,11 @@ sessionio.on('connection', (socket) => {
             });
         });
 
-        TallySocket.on('disconnect', function(){
+        TallySocket.on('disconnect', function() {
             console.log("Disconnected from server");
-            programLed.write( 0 );
-            previewLed.write( 0 );
+            programLed.write(0);
+            previewLed.write(0);
             TallySocket = null;
         });
-	});
+    });
 });
